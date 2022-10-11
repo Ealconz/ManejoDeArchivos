5b2d9171d2b9b46d7bce912288510faafe7b0713
Ealconz <114057061+Ealconz@users.noreply.github.com>
2022-10-11 10:07:52 +0200

    manejoArchivos

---
+++ ManejoArchivos/src/ManejoArchivos/ManejoArchivos.java
@@ -0,0 +1,16 @@
+/*
+ * Nuestra clase de manejo de archivos va a tener los siguientes metodos
+    - Crear Archivo
+    - Guardar informacion en un archivo
+    - Eliminar informacion de un archivo
+    - Eliminar un archivo
+ */
+package ManejoArchivos;
+
+/**
+ *
+ * @author elias
+ */
+public class ManejoArchivos {
+    
+}
